a2c1282e41427196ca8b096f6d015184d44e46da
Jonathan Diehl <jonathan.diehl@gmail.com>
2020-01-01 12:02:53 +0100

    bugfix

--- server.js
+++ server.js
@@ -32,7 +32,11 @@ function use(extension) {
     return extension.forEach(use)
   }
   if (extension.middleware) middlewares.push(extension.middleware)
-  if (extension.resolvers) Object.assign(resolvers, extension.resolvers)
+  if (extension.resolvers) {
+    for (const key of Object.keys(extension.resolvers)) {
+      resolvers[key] = Object.assign(resolvers[key] || {}, extension.resolvers[key])
+    }
+  }
   if (extension.queries) Object.assign(resolvers.Query, extension.queries)
   if (extension.mutations) Object.assign(resolvers.Mutation, extension.mutations)
   if (extension.typeDefs) typeDefs.push(extension.typeDefs)
