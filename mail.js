e20be20bc4037d85afac1ab2c11fc4f684264c83
Jonathan Diehl <jonathan.diehl@gmail.com>
2020-05-23 18:53:52 +0200

    added nodemailer

--- mail.js
+++ mail.js
@@ -1,11 +1,45 @@
 const sgMail = require('@sendgrid/mail')
+const nodemailer = require('nodemailer')
 const { compileFile } = require('pug')
 const { getEnv } = require('./util')
 
 const SENDGRID_API_KEY = getEnv('SENDGRID_API_KEY', 'string', null)
 const SENDGRID_FROM = getEnv('SENDGRID_FROM', 'string', null)
 
+const MAIL_HOST = getEnv('MAIL_HOST', 'string', null)
+const MAIL_FROM = getEnv('MAIL_FROM', 'string', null)
+const MAIL_PASS = getEnv('MAIL_PASS', 'string', null)
+const MAIL_RATE = getEnv('MAIL_RATE', 'int', 10)
+const MAIL_DELTA = getEnv('MAIL_DELTA', 'int', 1000)
+
+// set up send grid
+// todo: switch to https://sendgrid.com/blog/sending-email-nodemailer-sendgrid/
 if (SENDGRID_API_KEY) sgMail.setApiKey(SENDGRID_API_KEY)
+
+// set up nodemailer
+let transport
+if (MAIL_HOST) {
+  console.log('config', {
+    host: MAIL_HOST,
+    auth: {
+      user: MAIL_FROM,
+      pass: MAIL_PASS
+    },
+    pool: true,
+    rateLimit: MAIL_RATE,
+    rateDelta: MAIL_DELTA
+  })
+  transport = nodemailer.createTransport({
+    host: MAIL_HOST,
+    auth: {
+      user: MAIL_FROM,
+      pass: MAIL_PASS
+    },
+    pool: true,
+    rateLimit: MAIL_RATE,
+    rateDelta: MAIL_DELTA
+  })
+}
 
 const templates = {}
 function addTemplate(name, file) {
@@ -13,9 +47,9 @@ function addTemplate(name, file) {
 }
 
 async function send(msg) {
-  if (!SENDGRID_API_KEY) throw new Error('SENDGRID_API_KEY not configured')
+  if (!SENDGRID_API_KEY && !MAIL_HOST) throw new Error('Email not configured')
 
-  msg.from = msg.from || SENDGRID_FROM
+  msg.from = msg.from || MAIL_FROM || SENDGRID_FROM
 
   // parse template
   if (msg.template) {
@@ -25,7 +59,7 @@ async function send(msg) {
   }
 
   console.log(`SEND MAIL TO:${msg.to} SUBJECT:${msg.subject} TEXT:${msg.text}`)
-  return sgMail.send(msg)
+  return transport ? transport.sendMail(msg) : sgMail.send(msg)
 }
 
 module.exports = {
